606ab74457144914d17f6d5057e4be1942bf8f97
Ankalakoti <C082043@exelonds.com>
2021-06-24 20:48:30 +0530

    Added a Procfile.

--- app.js
+++ app.js
@@ -1,66 +1,73 @@
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var http = require('http');
-var path = require("path");
-var bodyParser = require('body-parser');
-var balancefinderRouter = require('./routes/balancefinder');
-
+// Load node module and initialising the object
+var createError = require( 'http-errors' );
+var express = require( 'express' );
+var path = require( 'path' );
+var cookieParser = require( 'cookie-parser' );
+var logger = require( 'morgan' );
+var http = require( 'http' );
+var path = require( "path" );
+var bodyParser = require( 'body-parser' );
+var balancefinderRouter = require( './routes/balancefinder' );
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use( bodyParser.urlencoded(
+{
+  extended: false
+} ) );
 
 
 // environment variables. Setting the environment as development, which will be used to fetch the development config values.
 process.env.NODE_ENV = 'development';
 
 // Load request module
-var lodash = require('lodash');
+var lodash = require( 'lodash' );
 
 // loading config values
-var config = require('./config/config.json');
+var config = require( './config/config.json' );
 var defaultConfig = config.development;
 var environment = process.env.NODE_ENV || 'development';
-var environmentConfig = config[environment];
-var finalConfig = lodash.merge(defaultConfig, environmentConfig);
+var environmentConfig = config[ environment ];
+var finalConfig = lodash.merge( defaultConfig, environmentConfig );
 
 // as a best practice all global variables should be referenced via global syntax and their names should always begin with g
 
 global.gConfig = finalConfig;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set( 'views', path.join( __dirname, 'views' ) );
+app.set( 'view engine', 'jade' );
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use( logger( 'dev' ) );
+app.use( express.json() );
+app.use( express.urlencoded(
+{
+  extended: false
+} ) );
+app.use( cookieParser() );
+app.use( express.static( path.join( __dirname, 'public' ) ) );
 
-app.use(global.gConfig.app_uri_gettokenbalance, balancefinderRouter);
+app.use( global.gConfig.app_uri_gettokenbalance, balancefinderRouter );
 
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use( function ( req, res, next )
+{
+  next( createError( 404 ) );
+} );
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use( function ( err, req, res, next )
+{
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get( 'env' ) === 'development' ? err :
+  {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status( err.status || 500 );
+  res.render( 'error' );
+} );
 
 
 
--- routes/balancefinder.js
+++ routes/balancefinder.js
@@ -1,63 +1,74 @@
 // Load alchemy module and initialising the object
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(
-  "https://eth-mainnet.alchemyapi.io/v2/lL7JKoLn0pxMORx9WwWUteHZa2Ly4aOk");
+const
+{
+  createAlchemyWeb3
+} = require( "@alch/alchemy-web3" );
+
 
 // Load express module and initialising the object
-var express = require('express');
+var express = require( 'express' );
 var app = express();
 var router = express.Router();
-const url = require('url');
+const url = require( 'url' );
 
 // Load request module
-var request = require('request');
+var request = require( 'request' );
 
 // environment variables. Setting the environment as development, which will be used to fetch the development config values.
 process.env.NODE_ENV = 'development';
 
 // Load request module
-var lodash = require('lodash');
+var lodash = require( 'lodash' );
 
 // loading config values
-const config = require('../config/config.json');
+const config = require( '../config/config.json' );
 const defaultConfig = config.development;
 const environment = process.env.NODE_ENV || 'development';
-const environmentConfig = config[environment];
-const finalConfig = lodash.merge(defaultConfig, environmentConfig);
+const environmentConfig = config[ environment ];
+const finalConfig = lodash.merge( defaultConfig, environmentConfig );
 
 // as a best practice all global variables should be referenced via global syntax and their names should always begin with g
 global.gConfig = finalConfig;
 
-// this function routes the HTTP GET Requests to the provide URL path which is being specified with the specified access token and access keys
-app.get(global.gConfig.app_uri_gettokenbalance, function(req, res) {
-  // Reading the tweet id passed in the request URL
-	var address = req.query.address;
-	var tokenAddress = [];
-	tokenAddress.push(req.query.tokenAddress);
-
-       const isItDoneYet = new Promise((resolve, reject) => {
-   web3.alchemy.getTokenBalances(address, tokenAddress).then( balances => {
-	    
- 	   balances = JSON.parse(JSON.stringify(balances, 0, 2));
-	  console.log(JSON.stringify(balances, 0, 2));
-   res.end(JSON.stringify(balances, 0, 2));
+// Initialisng the web3 object
+const web3 = createAlchemyWeb3(
+  global.gConfig.url_twitter + global.gConfig.api_key );
   
-	
-});
-	   })
-	   .catch(err => {
-      console.error(err)
-    })
-	   
-});
+// this function routes the HTTP GET Requests to the provide URL path which is being specified with the specified access token and access keys
+app.get( global.gConfig.app_uri_gettokenbalance, function ( req, res )
+{
+  // Reading the wallet address and token address passed in the request URL
+  var address = req.query.address;
+  var tokenAddress = [];
+  tokenAddress.push( req.query.tokenAddress );
+
+  const isItDoneYet = new Promise( ( resolve, reject ) =>
+    {
+      web3.alchemy.getTokenBalances( address, tokenAddress ).then( balances =>
+      {
+
+        balances = JSON.parse( JSON.stringify( balances, 0, 2 ) );
+        console.log( JSON.stringify( balances, 0, 2 ) );
+        res.end( JSON.stringify( balances, 0, 2 ) );
+
+
+      } );
+    } )
+    .catch( err =>
+    {
+      console.error( err )
+    } )
+
+} );
 
 // setting the application to listen on port configured in the config file
-var server = app.listen(process.env.PORT || global.gConfig.node_port, function() {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Fetch Tweet Details app listening at %s", port)
+var server = app.listen( process.env.PORT || global.gConfig.node_port, function ()
+{
+  var host = server.address().address
+  var port = server.address().port
+  console.log( "Fetch getTokenBalance Details app listening at %s", port )
 
-})
+} )
 
 
 module.exports = router;
